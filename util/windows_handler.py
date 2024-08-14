fef52590f39674120a21362f2e6a9bfbda1c7c2e
lfq <77376840+BUPTlfq@users.noreply.github.com>
2024-08-15 04:20:16 +0800

    Add files via upload

---
+++ util/windows_handler.py
@@ -0,0 +1,11 @@
+from util.abstract_os_handler import AbstractOSHandler
+import torch.multiprocessing as mp
+
+
+class WindowsHandler(AbstractOSHandler):
+
+    def create_mp_queue(self) -> mp.Queue:
+        return mp.Queue()
+
+    def get_num_workers(self) -> int:
+        return 0
